fc20cfaf8f1d5dde140a51fb02fbf0d38bd2517e
JARS3N <JARS3N@users.noreply.github.com>
2025-07-13 19:38:45 -0400

    Update optimize.sh

--- optimize.sh
+++ optimize.sh
@@ -1,1 +1,21 @@
-.
+#!/bin/bash
+# Clean up
+sudo apt autoremove -y
+sudo apt clean
+
+# Disable animations for snappier performance
+xfconf-query -c xfwm4 -p /general/use_compositing -s false
+
+# Enable Plank on startup
+mkdir -p ~/.config/autostart
+cat <<EOF > ~/.config/autostart/plank.desktop
+[Desktop Entry]
+Type=Application
+Exec=plank
+Hidden=false
+NoDisplay=false
+X-GNOME-Autostart-enabled=true
+Name=Plank
+EOF
+
+echo "Optimization complete."
