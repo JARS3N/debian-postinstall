7c0cbe8093aad562755deaa9440c7765a2ca1646
JARS3N <JARS3N@users.noreply.github.com>
2025-07-13 19:48:05 -0400

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,10 +1,1 @@
-#!/bin/bash
-# Install base packages and XFCE desktop
-sudo apt update && sudo apt upgrade -y
-sudo apt install -y xfce4 xfce4-goodies plank synaptic gnome-disk-utility neofetch htop git curl
 
-# Optional: Install your preferred browser, terminal, etc.
-# sudo apt install -y firefox
-
-# Run optimization after install
-bash optimize.sh
