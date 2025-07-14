a0a2726163165d0e22252b2b98397c8378bceb73
JARS3N <JARS3N@users.noreply.github.com>
2025-07-14 00:57:35 -0400

    Update apps.sh

--- apps.sh
+++ apps.sh
@@ -52,5 +52,10 @@ systemctl enable NetworkManager
 # Enable LightDM
 systemctl enable lightdm
 
+#install spotify
+curl -sS https://download.spotify.com/debian/pubkey_C85668DF69375001.gpg | sudo gpg --dearmor --yes -o /etc/apt/trusted.gpg.d/spotify.gpg
+echo "deb https://repository.spotify.com stable non-free" | sudo tee /etc/apt/sources.list.d/spotify.list
+sudo apt-get update && sudo apt-get install spotify-client
+
 # Done
 echo "Setup complete. Reboot recommended."
